466a06c82baa8248e960ea1e3ccb0676c82fdd89
Matthew Kelsey <kelsmj@Matthews-MacBook-Pro.local>
2012-07-07 10:36:36 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WildAboutWhippets::Application.config.secret_token = '549b48cd97060a2ea7c9be60d391ae9a78549d30b3a61d92700d159d9c868e6b91d8112ec33025fc96e107e22707709899d677c340002ddaf1842ef23c3ed692'
