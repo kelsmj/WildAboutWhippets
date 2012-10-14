6dc392780c24e21ce8f6535e1c57ed96ecd6bfd9
Matt Kelsey <matt@mkelsey.com>
2012-10-14 12:06:56 -0400

    Reorg

--- Gemfile
+++ Gemfile
@@ -16,9 +16,9 @@ group :assets do
   gem 'uglifier', '1.2.3'
 end
 
-gem 'jquery-rails', '2.0.0'
+gem "jquery-rails", "~> 2.1.3"
 
-gem "twitter-bootstrap-rails"
+gem "twitter-bootstrap-rails", "~> 2.1.3"
 
 group :production do
   gem 'pg', '0.12.2'
---
+++ app/assets/images/index.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+ 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"></meta>
+  <!-- fluid grid system framework files --> 
+  <link rel="stylesheet" href="stylesheets/fluid.gs.css" type="text/css" media="screen" title="no title" charset="utf-8"> 
+  <!--[if lt IE 8]><link rel="stylesheet" href="stylesheets/fluid.gs.lt_ie8.css" type="text/css" media="screen" title="no title" charset="utf-8"><![endif]--> 
+ 
+  <!-- site specific files --> 
+  <link rel="stylesheet" href="stylesheets/main.css" type="text/css" media="screen" title="no title" charset="utf-8"> 
+  <!--[if lt IE 8]><link rel="stylesheet" href="stylesheets/main.lt_ie8.css" type="text/css" media="screen" title="no title" charset="utf-8"><![endif]--> 
+	<link rel="stylesheet" type="text/css" href="skeleton/skeleton.css" />
+
+	<script src="skeleton/stuHover.js" type="text/javascript"></script>
+<META NAME="author" CONTENT="Matt Kelsey">
+<META NAME="description" CONTENT="Site dedicated to the versatile whippet">
+<META NAME="keywords" CONTENT="Whippets">
+<Title>Wild About Whippets - Often taking the road less traveled...</title>
+ 
+</head> 
+ 
+<body style="background:#665847"> 
+<div id="main" class="fluid_grid_layout" style="background:#3D362D"> 
+  
+  <div class="six_column section"> 
+    <div class="two column"> 
+      <div class="column_content"> 
+        <img src="walogosmall.png"/>
+      </div>
+    </div> 
+ 
+    <div class="four column"> 
+      <div class="column_content"> 
+      </div> 
+    </div> 
+    
+  </div> 
+  <!--NAV-->
+  <div class="six_column section">
+	<div class="six column">
+		<ul id="nav" style="border-right:0px;border-left:0px">
+			<li><a href="index.php">Home</a>
+	<ul>
+		<li><a href="index.php?content=/home/kelsey.ihtml">About Us</a></li>		
+		<li><a href="index.php?content=/family/family.ihtml">Family &amp; Friends</a></li>
+		<li><a href="index.php?content=/gallery/gallery.ihtml">Photos</a></li>
+	</ul>
+</li>
+<li><a href="#nogo">Currently Competing &#187;</a>
+	<ul>
+		<li><a href="index.php?content=/litters/nico_hayley/kenny.ihtml">Kenny P</a></li>
+		<li><a href="index.php?content=/ourdogs/fig/fig.ihtml">Fig</a></li>
+		<li><a href="index.php?content=/litters/nico_hayley/capone.ihtml">Capone</a></li>
+		<li><a href="index.php?content=/litters/nico_hayley/bullet.ihtml">Bullet</a></li>
+		<li><a href="index.php?content=/litters/nico_hayley/pistol.ihtml">Pistol</a></li>
+		<li><a href="index.php?content=/litters/parker_zinfandel/spencer.ihtml">Spencer</a></li>
+		<li><a href="index.php?content=/litters/nico_hayley/swish.ihtml">Swish</a></li>
+		<li><a href="index.php?content=/litters/syd_tigerlily/mason.ihtml">Mason</a></li>
+		<li><a href="index.php?content=/ourdogs/zinfandel/zinfandel.ihtml">Zinfandel</a></li>
+		<li><a href="index.php?content=/litters/zeke_vegas/hayley.ihtml">Hayley</a></li>
+		<li><a href="index.php?content=/ourdogs/zircon/zircon.ihtml">Zircon</a></li>
+		<li><a href="index.php?content=/litters/zeke_vegas/zandy.ihtml">Zandy</a></li>
+		<li><a href="index.php?content=/litters/zeke_vegas/zest.ihtml">Zest</a></li>
+		<li><a href="index.php?content=/litters/gateway_vegas/vitesse.ihtml">Vitesse</a></li>
+		<li><a href="index.php?content=/litters/gateway_vegas/maddie.ihtml">Maddie</a></li>
+		<li><a href="index.php?content=/litters/gateway_vegas/lucy.ihtml">Lucy</a></li>
+	</ul>
+</li>
+<li><a href="#nogo">Rising Stars &#187;</a>
+	<ul>
+		
+		<li><a href="index.php?content=/ourdogs/mia/mia.ihtml">Mia</a></li>
+		<li><a href="index.php?content=/litters/parker_zinfandel/nova.ihtml">Nova</a></li>
+		<li><a href="index.php?content=/litters/nico_hayley/lotus.ihtml">Lotus</a></li>
+		<li><a href="index.php?content=/litters/parker_zinfandel/dasha.ihtml">Dasha</a></li>
+		<li><a href="index.php?content=/litters/parker_zinfandel/ninja.ihtml">Ninja</a></li>
+		<li><a href="index.php?content=/litters/parker_zinfandel/zoey.ihtml">Zoey</a></li>
+		<li><a href="index.php?content=/litters/parker_zinfandel/hoax.ihtml">Hoax</a></li>
+		
+		
+	</ul>
+</li>
+<li><a href="#nogo">Retired &#187;</a>
+	<ul>
+		<li><a href="index.php?content=/ourdogs/vegas/vegas.ihtml">Vegas</a></li>
+		<li><a href="index.php?content=/ourdogs/tigerlily/tigerlily.ihtml">Tiger Lily</a></li>
+		<li><a href="index.php?content=/ourdogs/zeppelin/zeppelin.ihtml">Zeppelin</a></li>
+		<li><a href="index.php?content=/ourdogs/birdie/birdie.ihtml">Birdie</a></li>
+		<li><a href="index.php?content=/ourdogs/syd/syd.ihtml">Syd</a></li>
+	<li><a href="#nogo">In Memory &#187;</a>
+		<ul>
+			<li><a href="index.php?content=/ourdogs/genie/genie.ihtml">Genie</a></li>
+			<li><a href="index.php?content=/ourdogs/scotch/scotch.ihtml">Scotch</a></li>
+			<li><a href="index.php?content=/litters/syd_tigerlily/lilsyd.ihtml">Lil Syd</a></li>
+			<li><a href="index.php?content=/litters/gateway_vegas/lyla.ihtml">Lyla</a></li>
+		</ul>
+	</li>
+	</ul>
+</li>
+<li><a href="#nogo">Litters &#187;</a>
+	<ul>
+		<li><a href="index.php?content=/litters/futurelitters.ihtml">Future Plans</a></li>
+		<li><a href="index.php?content=/litters/wwlitters.ihtml">Bred By WildAbout Whippets! &#187;</a>
+			<ul>
+				<li><a href="index.php?content=/litters/parker_zinfandel/parkerzinfandel.ihtml">Parker x Zinfandel 2010 &#187;</a>
+				<li><a href="index.php?content=/litters/nico_hayley/nicohayley.ihtml">Nico x Hayley 2010 &#187;</a>					
+				<li><a href="index.php?content=/litters/gateway_vegas/gatewayvegas.ihtml">Gateway x Vegas 2007 &#187;</a>
+					<ul>
+						<li><a href="index.php?content=/ourdogs/soho/soho.ihtml">Soho</a></li>
+						<li><a href="index.php?content=/litters/gateway_vegas/vitesse.ihtml">Vitesse</a></li>
+						<li><a href="index.php?content=/litters/gateway_vegas/maddie.ihtml">Maddie</a></li>
+						<li><a href="index.php?content=/litters/syd_tigerlily/lucy.ihtml">Lucy</a></li>
+						<li><a href="index.php?content=/litters/gateway_vegas/beefie.ihtml">Beefie</a></li>
+						<li><a href="index.php?content=/litters/gateway_vegas/tuli.ihtml">Tuli</a></li>
+						<li><a href="index.php?content=/litters/syd_tigerlily/stella.ihtml">Stella</a></li>
+						<li><a href="index.php?content=/litters/gateway_vegas/lyla.ihtml">Lyla</a></li>
+						<li><a href="index.php?content=/litters/gateway_vegas/checker.ihtml">Checker</a></li>
+					</ul>
+				</li>
+				<li><a href="index.php?content=/litters/zeke_vegas/zekevegas.ihtml">Zeke x Vegas 2005 &#187;</a>
+					<ul>
+						<li><a href="index.php?content=/ourdogs/zinfandel/zinfandel.ihtml">Zinfandel</a></li>
+						<li><a href="index.php?content=/ourdogs/zazu/zazu.ihtml">Zazu</a></li>
+						<li><a href="index.php?content=/ourdogs/ezrin/ezrin.ihtml">Ezrin</a></li>
+						<li><a href="index.php?content=/litters/zeke_vegas/bravizzimo.ihtml">Bravizzimo</a></li>
+						<li><a href="index.php?content=/litters/zeke_vegas/hayley.ihtml">Hayley</a></li>
+						<li><a href="index.php?content=/litters/zeke_vegas/zest.ihtml">Zest</a></li>
+						<li><a href="index.php?content=/litters/zeke_vegas/zandy.ihtml">Zandy</a></li>
+						<li><a href="index.php?content=/ourdogs/zircon/zircon.ihtml">Zircon</a></li>
+					</ul>
+				</li>
+				<li><a href="index.php?content=/litters/syd_tigerlily/sydtigerlily.ihtml">Syd x Tiger Lily 2004 &#187;</a>
+					<ul>
+						<li><a href="index.php?content=/litters/syd_tigerlily/lilsyd.ihtml">Lil Syd</a></li>
+						<li><a href="index.php?content=/litters/syd_tigerlily/hemi.ihtml">Hemi</a></li>
+						<li><a href="index.php?content=/litters/syd_tigerlily/wonder.ihtml">Wonder</a></li>
+						<li><a href="index.php?content=/litters/syd_tigerlily/mason.ihtml">Mason</a></li>
+					</ul>
+				</li>
+				<li><a href="index.php?content=/litters/mouff_genie/mouffgenie.ihtml">Mouff x Genie 2003 &#187;</a>
+					<ul>
+						<li><a href="index.php?content=/ourdogs/zeppelin/zeppelin.ihtml">Zeppelin</a></li>
+						<li><a href="index.php?content=/ourdogs/syd/syd.ihtml">Syd</a></li>
+						<li><a href="index.php?content=/ourdogs/rinna/rinna.ihtml">Rinna</a></li>
+						<li><a href="index.php?content=/litters/mouff_genie/rev.ihtml">Rev</a></li>
+						<li><a href="index.php?content=/ourdogs/birdie/birdie.ihtml">Birdie</a></li>
+						<li><a href="index.php?content=/litters/mouff_genie/lark.ihtml">Lark</a></li>
+					</ul>
+				</li>
+				<li><a href="index.php?content=/litters/willie_genie/williegenie.ihtml">Willie x Genie 2002 &#187;</a>
+					<ul>
+						<li><a href="index.php?content=/ourdogs/tigerlily/tigerlily.ihtml">Tiger Lily</a></li>
+						<li><a href="index.php?content=/litters/willie_genie/daisy.ihtml">Daisy</a></li>
+						<li><a href="index.php?content=/litters/willie_genie/stryder.ihtml">Stryder &amp; Casino</a></li>
+						<li><a href="index.php?content=/litters/willie_genie/flirt.ihtml">Flirt</a></li>
+						<li><a href="index.php?content=/litters/willie_genie/beemer.ihtml">Beemer</a></li>
+						<li><a href="index.php?content=/litters/willie_genie/buckley.ihtml">Buckley</a></li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+		<li><a href="index.php?content=/litters/familylitters.ihtml">Family litters &#187;</a>
+					<ul>
+						<li><a href="index.php?content=/litters/reach_vi/reachvi.ihtml">Reach x Vi 2011 &#187;</a>
+						<li><a href="index.php?content=/litters/bonzer_lucy/bonzerlucy.ihtml">Bonzer x Lucy 2011 &#187;</a>
+						<ul>
+								<li><a href="index.php?content=/litters/bonzer_lucy/summer.ihtml">Summer</a></li>
+								<li><a href="index.php?content=/litters/bonzer_lucy/caesar.ihtml">Caesar</a></li>
+								<li><a href="index.php?content=/litters/bonzer_lucy/spirit.ihtml">Spirit</a></li>
+								<li><a href="index.php?content=/litters/bonzer_lucy/prada.ihtml">Prada</a></li>
+								<li><a href="index.php?content=/litters/bonzer_lucy/lexi.ihtml">Lexi</a></li>
+							</ul>
+						</li>
+							
+						<li><a href="index.php?content=/litters/lp_tuli/lptuli.ihtml">L.P. x Tuli 2009</a>
+							<ul>
+								<li><a href="index.php?content=/litters/lp_tuli/finnegan.ihtml">Finnegan</a></li>
+								<li><a href="index.php?content=/litters/lp_tuli/pipper.ihtml">Pipper</a></li>
+							</ul>
+						</li>
+						<li><a href="index.php?content=/litters/huey_tuli/hueytuli.ihtml">Huey x Tuli 2010</a>
+						<li><a href="index.php?content=/litters/zandy_derby/zandyderby.ihtml">Zandy x Derby 2008 &#187;</a>
+							<ul>
+								<li><a href="index.php?content=/litters/zandy_derby/lobo.ihtml">Lobo</a></li>
+								<li><a href="index.php?content=/litters/zandy_derby/chico.ihtml">Chico</a></li>
+								<li><a href="index.php?content=/litters/zandy_derby/carly.ihtml">Carly</a></li>
+								<li><a href="index.php?content=/litters/zandy_derby/torres.ihtml">Torres</a></li>
+								<li><a href="index.php?content=/litters/zandy_derby/panita.ihtml">Fay</a></li>
+							</ul>
+						</li>
+						</li>
+					</ul>
+		</li>
+	</ul>
+</li>
+<li><a href="#nogo">Puppies &#187;</a>
+	<ul>
+		<li><a href="index.php?content=/puppies/whelping.ihtml">In the Whelping Box</a></li>			
+		<li><a href="WhippetQuest.doc">Puppy Questionnaire</a></li>
+		<li><a href="index.php?content=/puppies/protocol.ihtml">Puppy Protocol</a></li>
+		<li><a href="index.php?content=/puppies/puppycam.ihtml">PUPPYCAM!!</a></li>
+	</ul>
+</li>
+<li><a href="index.php?content=/links/links.ihtml">Great Links</a>
+</li>
+
+</ul>
+	</div>
+  </div>
+ 
+ <!--END NAV-->
+ <div class="six_column section"> 
+    <div class="six column"> 
+      <div class="column_content"> 
+<?php 
+$str = $_SERVER['QUERY_STRING'];
+parse_str($str);
+//print strlen($content);
+if (strlen($content)>0 && substr($content, -6) == '.ihtml') {
+	require_once $myDirPath . 'content' . $content;
+}
+else
+{
+	require_once $myDirPath . 'content/home/home.ihtml';
+}
+?></div>
+    </div>     
+  </div> 
+</div>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-7736901-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+</body>
+</html>
